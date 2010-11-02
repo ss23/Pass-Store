af54335cb74115a4595bdfe8f1cb4ab4d66989e7
Stephen Shkardoon <ss23@mx.ss23.geek.nz>
2010-11-02 18:10:21 +1100

    Modified it to automatically add users to the database

--- generateUser.php
+++ generateUser.php
@@ -3,14 +3,17 @@
 
 if (count($argv) != 3) {
 	echo "Example Usage - ./generateUser username password\n";
-	echo "Or, if you want spaces in your password - ./generateUser username 'password with spaces'";
+	echo "Or, if you want spaces in your password - ./generateUser username 'password with spaces'\n";
+	die();
 }
 
 require ('html/include/functions/Sanitize.php');
 require ('html/include/functions/User.php');
 
-echo "Username: " . s($argv[1]) . "\n";
-echo "Hashed Password: " . user_hash($argv[2], $argv[1]) . "\n";
+$Username = s($argv[1]);
+$Password = user_hash($argv[2], $argv[1]);
+echo "Username: " . $Username . "\n";
+echo "Hashed Password: " . $Password . "\n";
 
 echo "Generating OpenSSL Keys...\n";
 
@@ -28,5 +31,22 @@ file_put_contents('./keys/' . $argv[1] . '.pem', $PrivateKey);
 file_put_contents('./keys/' . $argv[1] . '.pub', $PublicKey);
 
 echo "Certificates generated\n";
-echo "Remeber to manually add this user to your database\n";
+
+// Start the connection to the database
+require ('config/config.php');
+require ('html/include/MyPDO.php');
+$pdo = new MyPDO();
+
+$stmt = $pdo->prepare('insert into `users` (
+		`username`, `password`
+		) values (
+		:username, :password
+		)');
+$stmt->bindParam(':username', $Username);
+$stmt->bindParam(':password', $Password);
+if (!$stmt->execute()) {
+	echo "Automatic entry to database failed. Please do this manually.\n";
+} else {
+	echo "User automatically inserted into database.\n";
+}
 ?>
